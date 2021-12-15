3c672970b9116f0ccfed48bb17c0bfb31c2bd311
Piotr Wilkosz <pwilkosz99@gmail.com>
2021-12-15 10:47:04 +0100

    OBJ loading

--- WDLabs/WDLabs/WDLabs.cpp
+++ WDLabs/WDLabs/WDLabs.cpp
@@ -9,6 +9,7 @@
 #include <SFML/System/Time.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS
 #include "stb_image.h"
 
 
@@ -88,6 +89,8 @@ static float lightType = 1.0f;
 
 bool firstMouse = true;
 int lastX, lastY;
+
+int vertSize;
 
 //kąty do kontroli nachylenia kamery
 double yaw = -90;
@@ -274,6 +277,68 @@ void loadTexture() {
 	stbi_image_free(data);
 }
 
+bool loadOBJ(const char* path, std::vector < glm::vec3 >& out_vertices, std::vector < glm::vec2 >& out_uvs, std::vector < glm::vec3 >& out_normals)
+{
+	std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
+	std::vector< glm::vec3 > temp_vertices;
+	std::vector< glm::vec2 > temp_uvs;
+	std::vector< glm::vec3 > temp_normals;
+
+	FILE* file = fopen(path, "r");
+	if (file == NULL) {
+		printf("Impossible to open the file !\n");
+		return false;
+	}
+
+	while (1) {
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
+
+		if (strcmp(lineHeader, "v") == 0) {
+			glm::vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			temp_vertices.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			glm::vec2 uv;
+			fscanf(file, "%f %f\n", &uv.x, &uv.y);
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			glm::vec3 normal;
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			if (matches != 9) {
+				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+				return false;
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			uvIndices.push_back(uvIndex[0]);
+			uvIndices.push_back(uvIndex[1]);
+			uvIndices.push_back(uvIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
+		}
+	}
+	// For each vertex of each triangle
+	for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+		unsigned int vertexIndex = vertexIndices[i];
+		glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+		out_vertices.push_back(vertex);
+	}
+}
+
 int main()
 {
 	srand((unsigned)time(NULL));
@@ -314,7 +379,16 @@ int main()
 	loadTexture();
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// Read our .obj file
+	std::vector< glm::vec3 > verticesOBJ;
+	std::vector< glm::vec2 > uvs;
+	std::vector< glm::vec3 > normals; // Won't be used at the moment.
+	bool res = loadOBJ("ChairAndTable.obj", verticesOBJ, uvs, normals);
+	vertSize = verticesOBJ.size();
+
+	glBufferData(GL_ARRAY_BUFFER, verticesOBJ.size() * sizeof(glm::vec3), &verticesOBJ[0], GL_STATIC_DRAW);
 
 	// Utworzenie i skompilowanie shadera wierzchołków
 	GLuint vertexShader =
@@ -388,7 +462,7 @@ int main()
 	sf::Time time;
 
 	// Ustawienie tekstury na bierzaca
-	glBindTexture(GL_TEXTURE_2D, texture1);
+	//glBindTexture(GL_TEXTURE_2D, texture1);
 
 	glm::vec3 NewPosition;
 
@@ -501,7 +575,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Narysowanie figury
-		glDrawArrays(primitive, 0, 36);
+		glDrawArrays(primitive, 0, vertSize);
 		// Wymiana buforów tylni/przedni
 		window.display();
 	}
